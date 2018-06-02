978e161b65d26eb7fcaceff87b5cf16b04542e10
Shirly Niego <shirlyn995@gmail.com>
2018-06-02 15:21:01 +0300

    activity now workes with + button (ingredients only)

---
+++ app/src/main/java/com/example/cookit/Model/IngredientAdapter.java
@@ -0,0 +1,66 @@
+package com.example.cookit.Model;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.cookit.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.ViewHolder> {
+
+    public List<String> quantities = new ArrayList<>();
+    public List<String> descriptions = new ArrayList<>();
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.ingridient_list_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if (position >= quantities.size())
+            return;
+        holder.quantityEditText.setText(quantities.get(position), TextView.BufferType.EDITABLE);
+        holder.descriptionEditText.setText(descriptions.get(position), TextView.BufferType.EDITABLE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return quantities.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        final EditText descriptionEditText;
+        final EditText quantityEditText;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            quantityEditText = itemView.findViewById(R.id.quantityEditText);
+            quantityEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean hasFocus) {
+                    if(!hasFocus)
+                        quantities.set(getLayoutPosition(),quantityEditText.getText().toString());
+                }
+            });
+
+            descriptionEditText = itemView.findViewById(R.id.descriptionEditText);
+            descriptionEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean hasFocus) {
+                    if(!hasFocus)
+                        descriptions.set(getLayoutPosition(),descriptionEditText.getText().toString());
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/cookit/UploadRecipeActivity.java
+++ app/src/main/java/com/example/cookit/UploadRecipeActivity.java
@@ -1,82 +1,32 @@
 package com.example.cookit;
 
-import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
+
+import com.example.cookit.Model.IngredientAdapter;
 
 public class UploadRecipeActivity extends AppCompatActivity {
-    private final int DESCRIPTION_ID_ADDITION = 300;
-
-    private int ingredientsNum = 1;
-    private int preparationNum = 1;
-
-    private View.OnFocusChangeListener focusListener;
-    private ConstraintLayout cl;
-
+    private IngredientAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_recipe);
 
-        cl = (ConstraintLayout)findViewById(R.id.consLayout);
-        focusListener = new View.OnFocusChangeListener() {
+        adapter = new IngredientAdapter();
 
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus && ((EditText) findViewById(R.id.description1)).getText().length() != 0) {
-                    addLine(v);
-                }
-            }
-        };
-
-        ((EditText)findViewById(R.id.description1)).setOnFocusChangeListener(focusListener);
+        RecyclerView rv = ((RecyclerView) findViewById(R.id.my_recycler_view));
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(adapter);
+        addNewIngredient(null);
     }
 
-    public void addLine(View v) {
-        ingredientsNum++;
-
-        EditText quantityText = getQuantityEditText(v);
-
-        cl.addView(quantityText);
-        cl.addView(getDescriptionEditText(v, quantityText));
-    }
-
-    @NonNull
-    private EditText getQuantityEditText(View v) {
-        ConstraintLayout.LayoutParams p = new ConstraintLayout.LayoutParams(270, ViewGroup.LayoutParams.WRAP_CONTENT);
-        p.topMargin = 6;
-        p.topToBottom = v.getId();
-        p.setMarginStart(48);
-        p.startToStart = cl.getId();
-        p.setMarginEnd(0);
-
-        EditText quantityText = new EditText(this);
-        quantityText.setLayoutParams(p);
-        quantityText.setEms(10);
-        quantityText.setHint(R.string.quantity);
-        quantityText.setId(ingredientsNum);
-        return quantityText;
-    }
-
-    @NonNull
-    private EditText getDescriptionEditText(View v, EditText quantity) {
-        ConstraintLayout.LayoutParams p = new ConstraintLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
-        p.setMarginStart(0);
-        p.startToEnd = quantity.getId();
-        p.setMarginEnd(0);
-        p.endToEnd = cl.getId();
-        p.baselineToBaseline = quantity.getId();
-
-        EditText descriptionText = new EditText(this);
-        descriptionText.setLayoutParams(p);
-        descriptionText.setEms(10);
-        descriptionText.setHint(R.string.description);
-        descriptionText.setId(ingredientsNum + DESCRIPTION_ID_ADDITION);
-        return descriptionText;
+    public void addNewIngredient(View view) {
+        adapter.quantities.add("");
+        adapter.descriptions.add("");
+        adapter.notifyDataSetChanged();
     }
 }
